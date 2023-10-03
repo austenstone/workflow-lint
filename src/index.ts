6b52cef2b7feceef2dd8691fef286091c1728e98
Austen Stone <austenstone@github.com>
2023-10-03 12:18:56 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { getInput, info } from "@actions/core";
+import { getOctokit } from "@actions/github";
+
+interface Input {
+  token: string;
+}
+
+export function getInputs(): Input {
+  const result = {} as Input;
+  result.token = getInput("github-token");
+  return result;
+}
+
+const run = async (): Promise<void> => {
+  const input = getInputs();
+  const octokit = getOctokit(input.token);
+
+  const {
+    viewer: { login },
+  }: any = await octokit.graphql(`{ 
+    viewer {
+      login
+    }
+  }`);
+
+  info(`Hello, ${login}!`);
+};
+
+run();
