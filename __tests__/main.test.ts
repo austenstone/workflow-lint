4f10ad0752fa0af3b6ba8f6b64507de290abcb5f
Austen Stone <austenstone@github.com>
2023-10-06 11:28:05 -0400

    tmp

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -7,7 +7,9 @@ const addInput = (key, value) => process.env[`INPUT_${key.replace(/ /g, '-').toU
 
 const input: any = {
   'github-token': process.env.GITHUB_TOKEN,
-  organization: process.env.ORGANIZATION || 'octoaustenstone'
+  organization: process.env.ORGANIZATION || 'octoaustenstone',
+  owner: process.env.OWNER || 'austenstone',
+  repo: process.env.REPO || 'workflow-lint',
 }
 
 test('test run', () => {
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,45 @@
-import { getInput, info } from "@actions/core";
-import { getOctokit } from "@actions/github";
+import { getInput } from "@actions/core";
+import { NoOperationTraceWriter, parseWorkflow } from "@actions/workflow-parser";
+// import octokit
+import { Octokit } from "@octokit/rest";
+import { readFileSync } from "fs";
 
 interface Input {
   token: string;
+  owner: string;
+  repo: string;
 }
 
 export function getInputs(): Input {
   const result = {} as Input;
   result.token = getInput("github-token");
+  result.owner = getInput('owner');
+  result.repo = getInput('repo');
   return result;
 }
 
 const run = async (): Promise<void> => {
-  const input = getInputs();
-  const octokit = getOctokit(input.token);
+  const inputs = getInputs();
+  // get a list of changed files in this PR using octokit
+  const octokit = new Octokit({ auth: inputs.token });
+  const { data } = await octokit.pulls.listFiles({
+    owner: "microsoft",
+    repo: "vscode",
+    pull_number: 1234
+  });
+  // filter the list of changed files to only include workflow files
+  const workflowFiles = data.filter(file => file.filename.endsWith(".yml"));
+  // read workflow file content
+  workflowFiles.map(file => {
+    return readFileSync(file.filename, "utf8");
+  });
 
-  const {
-    viewer: { login },
-  }: any = await octokit.graphql(`{ 
-    viewer {
-      login
-    }
-  }`);
+  const result = parseWorkflow({
+    name: '123',
+    content: '123'
+  }, new NoOperationTraceWriter());
 
-  info(`Hello, ${login}!`);
+  console.log(result);
 };
 
 run();
