8459a7a587b1d66436caee4d92f19f055743a464
Austen Stone <austenstone@github.com>
2023-10-25 16:30:19 -0400

    test

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import { getInput, setOutput, setFailed, startGroup, endGroup } from "@actions/core";
-import { context } from "@actions/github";
-import { NoOperationTraceWriter, parseWorkflow } from "@actions/workflow-parser";
-// import octokit
-import { Octokit } from "@octokit/rest";
-import { readFileSync, readdirSync } from "fs";
-import { join } from "path";
+import { getInput, setOutput, setFailed, startGroup, endGroup, error } from '@actions/core';
+import { NoOperationTraceWriter, parseWorkflow } from '@actions/workflow-parser';
+import { readFileSync, readdirSync } from 'fs';
+import { join } from 'path';
 
 interface Input {
   token: string;
@@ -12,11 +9,11 @@ interface Input {
   repo: string;
   files: string;
 }
-const WORKFLOW_DIR = ".github/workflows";
+const WORKFLOW_DIR = '.github/workflows';
 
 export function getInputs(): Input {
   const result = {} as Input;
-  result.token = getInput("github-token");
+  result.token = getInput('github-token');
   result.owner = getInput('owner');
   result.repo = getInput('repo');
   result.files = getInput('files');
@@ -25,62 +22,34 @@ export function getInputs(): Input {
 
 const run = async (): Promise<void> => {
   const inputs = getInputs();
-  const octokit = new Octokit({ auth: inputs.token });
-  const check = await octokit.rest.checks.create({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    name: "GitHub Actions Workflow Lint",
-    head_sha: context.sha, // context.payload.pull_request?.head.sha || 
-    status: 'in_progress',
-  });
-
   const workflowFiles = inputs.files ?
     inputs.files.split(', ')
     :
     readdirSync(WORKFLOW_DIR)
-      .filter(name => name.endsWith(".yml") || name.endsWith(".yaml"))
+      .filter(name => name.endsWith('.yml') || name.endsWith('.yaml'))
       .map(name => join(WORKFLOW_DIR, name));
   startGroup(`Linting ${workflowFiles.length} workflow files`)
   workflowFiles.forEach(name => console.log(name));
   endGroup()
-  if (workflowFiles.length === 0) return setFailed("No workflow files found");
+  if (workflowFiles.length === 0) return setFailed('No workflow files found');
 
-  const results = workflowFiles.map(name => {
-    return {
-      path: name,
-      result: parseWorkflow({
-        name,
-        content: readFileSync(name, "utf8")
-      }, new NoOperationTraceWriter())
-    };
-  });
-  setOutput("results", JSON.stringify(results));
+  const results = workflowFiles.map(name => parseWorkflow({
+    name,
+    content: readFileSync(name, 'utf8')
+  }, new NoOperationTraceWriter()));
+  setOutput('results', JSON.stringify(results));
 
-  const annotations = results.reduce((acc, result) => {
-    const errors = result.result.context.errors.getErrors();
-    return acc.concat(errors.map(error => ({
-      path: result.path,
-      start_line: error.range?.start.line,
-      end_line: error.range?.end.line,
-      start_column: error.range?.start.column,
-      end_column: error.range?.end.column,
-      annotation_level: "failure",
-      message: error.message,
-      title: error.message.split('at')[0],
-    })));
-  }, [] as any[]);
-
-  await octokit.rest.checks.update({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    check_run_id: check.data.id,
-    conclusion: annotations.length > 0 ? 'failure' : 'success',
-    output: {
-      title: "GitHub Actions Workflow Lint",
-      summary: `${annotations.length} errors found in ${inputs.files}`,
-      annotations,
-    },
-  });
+  results.forEach(result => {
+    const errors = result.context.errors.getErrors();
+    errors.forEach(err => error(err.message, {
+      endColumn: err.range?.end.column,
+      startColumn: err.range?.start.column,
+      endLine: err.range?.end.line,
+      startLine: err.range?.start.line,
+      file: result.context.getFileTable()[0],
+      title: err.message.split('at')[0].trim(),
+    }));
+  });  
 };
 
 run();
