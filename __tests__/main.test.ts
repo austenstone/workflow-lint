9dd43dd36622e064c1884ad4f6a1b1d8fa5353ae
Austen Stone <austenstone@github.com>
2023-10-26 11:31:54 -0400

    ncu -u

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,10 +6,6 @@ import { test } from '@jest/globals';
 const addInput = (key, value) => process.env[`INPUT_${key.replace(/ /g, '-').toUpperCase()}`] = value
 
 const input: any = {
-  'github-token': process.env.GITHUB_TOKEN,
-  organization: process.env.ORGANIZATION || 'octoaustenstone',
-  owner: process.env.OWNER || 'austenstone',
-  repo: process.env.REPO || 'workflow-lint',
   files: process.env.WORKFLOW_PATH || '.github/workflows/workflow-invalid.yml',
 }
 
